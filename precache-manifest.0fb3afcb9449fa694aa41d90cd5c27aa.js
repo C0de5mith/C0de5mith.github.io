f1336b79ee5f02520a59732e8b3e7f805a3d3c80
C0de5mith <34120242+C0de5mith@users.noreply.github.com>
2020-10-11 14:04:15 -0500

    Updates

---
+++ precache-manifest.0fb3afcb9449fa694aa41d90cd5c27aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d294848302c8bf419393958ca654d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a34e7d54ffdd1be3f0e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "107d308a81e1d1d3563f",
+    "url": "/static/js/2.76a55f08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76a55f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a34e7d54ffdd1be3f0e",
+    "url": "/static/js/main.c0739241.chunk.js"
+  },
+  {
+    "revision": "0de845ed36c41eb5f90f",
+    "url": "/static/js/runtime-main.9a5e2046.js"
+  }
+]);
